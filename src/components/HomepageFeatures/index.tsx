dcfc4827e041320b63d4a5d57bc02e0c81f0e6d8
Nican <nican132@gmail.com>
2022-06-30 13:58:50 -0700

    feat(docs): More documentation on registration

--- src/components/HomepageFeatures/index.tsx
+++ src/components/HomepageFeatures/index.tsx
@@ -10,32 +10,31 @@ type FeatureItem = {
 
 const FeatureList: FeatureItem[] = [
   {
-    title: 'Easy to Use',
+    title: 'Designed for conventions',
     image: '/img/undraw_docusaurus_mountain.svg',
     description: (
       <>
-        Docusaurus was designed from the ground up to be easily installed and
-        used to get your website up and running quickly.
+        ConCat was designed from the ground up to be manage themed conventions, focusing
+        not only on attendees, but also volunteer and dealer managment.
       </>
     ),
   },
   {
-    title: 'Focus on What Matters',
+    title: 'Get the insights that you need',
     image: '/img/undraw_docusaurus_tree.svg',
     description: (
       <>
-        Docusaurus lets you focus on your docs, and we&apos;ll do the chores. Go
-        ahead and move your docs into the <code>docs</code> directory.
+        ConCat helps you keep all your information in your place. Manage vouchers, refunds,
+        badge printing, cashiers from one interface.
       </>
     ),
   },
   {
-    title: 'Powered by React',
+    title: 'For Convention, By Conventions',
     image: '/img/undraw_docusaurus_react.svg',
     description: (
       <>
-        Extend or customize your website layout by reusing React. Docusaurus can
-        be extended while reusing the same header and footer.
+        ConCat was made by avid convention goers, working closely with all departments.
       </>
     ),
   },
