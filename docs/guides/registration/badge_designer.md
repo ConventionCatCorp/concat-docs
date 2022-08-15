978991e90fb634e1613d4bfca73f130d62a934f7
Adam Walker <575410+Crashdoom@users.noreply.github.com>
2022-08-15 16:22:25 -0500

    fixup! minor fixes

--- docs/guides/registration/badge_designer.md
+++ docs/guides/registration/badge_designer.md
@@ -2,7 +2,7 @@
 sidebar_position: 3
 ---
 
-# Badge designer
+# Badge Designer
 
 <splitColumn>
   <div>
--- src/components/SplitColumn.tsx
+++ src/components/SplitColumn.tsx
@@ -7,7 +7,7 @@ interface Props {
 
 export default function SplitColumn({ children, childStyle }: Props) {
   return (
-    <div style={{ display: 'grid', gridTemplateColumns: 'repeat(auto-fit, minmax(300px, 1fr))', gridGap: '1rem', justifyItems: 'center' }}>
+    <div style={{ display: 'grid', gridTemplateColumns: 'repeat(auto-fit, minmax(300px, 1fr))', gridGap: '1rem' }}>
       {children.map((child, idx) => (
         <div style={childStyle} key={idx}>{child}</div>
       ))}
